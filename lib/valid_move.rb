c095314b76e25aae241051b461d59c4752a40c23
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-03 14:40:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@ def valid_move?(board,index)
   if position_taken?(board, index) && board[index].between(0,8)
     return true
   end
+  if position_taken == false && board[index] > 8
+    return false
 end
 
 def position_taken?(board, index)
